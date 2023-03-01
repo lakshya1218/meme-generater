676d732eda623e976f93fa4dcb01f4c6466eb5e8
Lakshya Jain <jainlakshya1218@gmail.com>
2023-03-01 17:05:41 +0530

    first comitting

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="https://www.figma.com/file/EH9DDpGgLUNin46mCYM43U/Meme-Generator-(Copy)?node-id=2%3A8&t=i4MhZLTYz6vEqH5I-4"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project"> Made By - Lakshya</h4>
+        </header>
+    )
+}
